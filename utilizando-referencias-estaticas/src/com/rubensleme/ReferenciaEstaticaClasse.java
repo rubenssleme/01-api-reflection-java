0321191d0359bc3be2de1fbd34b1472fbf59189c
Rubens Leme <rubenss.leme@gmail.com>
2020-10-17 03:30:16 -0300

    Utilizando referências estáticas

---
+++ utilizando-referencias-estaticas/src/com/rubensleme/ReferenciaEstaticaClasse.java
@@ -0,0 +1,13 @@
+package com.rubensleme;
+
+public class ReferenciaEstaticaClasse {
+
+    public static void main(String[] args){
+        Class<String> classe = String.class;
+        System.out.println(classe.getName());
+        imprimeNomeClasse(Integer.class);
+    }
+    public static void imprimeNomeClasse(Class<?> classe){
+        System.out.println("Chamado o método com " +classe.getName());
+    }
+}
